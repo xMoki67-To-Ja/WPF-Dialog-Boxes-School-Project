9b30ab322fc87b3576f4a6a18fb7543949d7c9a7
Dominik Pękal <pekal.dominik@zs6sobieski.edu.pl>
2024-01-02 11:28:56 +0100

    Dodaj pliki projektów.

---
+++ Okienka dialogowe/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System;
+using System.Windows;
+
+namespace Okienka_dialogowe
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowModalDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("To jest okno dialogowe modalne.", "Okno modalne", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void ShowNonModalDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            nonmodalWin nonModalWindow = new nonmodalWin();
+            nonModalWindow.Owner = this;
+            nonModalWindow.Show();
+        }
+        private void NonModalWindow_Opened(object sender, EventArgs e)
+        {
+            // Tutaj możesz wstawić kod, który będzie wykonywany po otwarciu niemodalnego okna
+            MessageBox.Show("Niemodalne okno jest teraz otwarte, możesz ingerować w pierwsze okno.");
+        }
+    }
+}
---
+++ Okienka dialogowe/nonmodal.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Okienka_dialogowe
+{
+    /// <summary>
+    /// Logika interakcji dla klasy nonmodal.xaml
+    /// </summary>
+    public partial class nonmodalWin : Window
+    {
+        public nonmodalWin()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
